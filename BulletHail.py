440ac199a3fd6d60622486dc6cff6a4fb1db7180
Petteri Kiiski <petteri.daniel.kiiski@gmail.com>
2023-04-18 16:18:39 -0700

    Comments

--- BulletHail.py
+++ BulletHail.py
@@ -22,6 +22,7 @@ class Bullet:
 #Map class
 class Map:
     def __init__(self):
+        #Setting up the rects for each room/hallway
         self.RectSet = [pygame.Rect(0, 0, 1360, 660)]#############01
         self.RectSet.append(pygame.Rect(540, 660, 272, 544))######02
         self.RectSet.append(pygame.Rect(1360, 194, 524, 272))#####03
@@ -43,9 +44,12 @@ class Map:
         self.RectSet.append(pygame.Rect(-1360, 660, 272, 524))####19
         self.RectSet.append(pygame.Rect(2408, -524, 272, 524))####20
         self.RectSet.append(pygame.Rect(2408, 660, 272, 524))#####21
+    #Debugging(run "python3 BulletHail.py -h" instead to enable)
     def show_hitboxes(self):
         for maprect in self.RectSet:
             pygame.draw.rect(canvas, (255, 0, 0), maprect, 1)
+
+    #checks if the player is in the Map
     def subrect(self, rect):
         colliding = 0
         for maprect in self.RectSet:
@@ -56,10 +60,14 @@ class Map:
             if subrect(maprect, rect):
                 return True
         return False
+
+    #The background moves to create the illusion of the player moving
     def move_player(self, x, y):
         for maprect in self.RectSet:
             maprect.left -= x
             maprect.top -= y
+
+    #Displays the Map
     def display(self):
         for maprect in self.RectSet:
             if maprect.width == 1360:
@@ -68,23 +76,36 @@ class Map:
                 canvas.blit(HorizontalHallway, maprect)
             elif maprect.width == 272:
                 canvas.blit(VerticalHallway, maprect)
+
+#More initialization
 world = Map()
 right = False
 left = False
 up = False
 down = False
 playerect = pygame.Rect(630, 230, 101, 174)
+
+#Subrect helper function for the Map class
 def subrect(rect, subrect):
     if subrect.left >= rect.left and subrect.right <= rect.right and subrect.top >= rect.top and subrect.bottom <= rect.bottom:
         return True
     return False
+
+#Another helper function for the Map class
 def partialsubrect(rect, subrect):
     if int(subrect.left >= rect.left) + int(subrect.right <= rect.right) + int(subrect.top >= rect.top) + int(subrect.bottom <= rect.bottom) >= 3:
         return True
+
+#Mainloop
 while True:
+    #Display everything
     canvas.fill((45, 30, 90))
     world.display()
     canvas.blit(avatar, playerect)
+    if "-h" in sys.argv or "--hitboxes" in sys.argv: # Display hitboxes
+        pygame.draw.rect(canvas, (0, 255, 0), playerect, 1)
+        world.show_hitboxes()
+    #Eventloop
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -111,10 +132,11 @@ while True:
                 down = False
             if event.key == K_UP:
                 up = False
+    #Moving the player here
     if right:
         world.move_player(10, 0)
-        if not world.subrect(playerect):
-            world.move_player(-10, 0)
+        if not world.subrect(playerect):# Is it in the map bounds?
+            world.move_player(-10, 0) # Move back if not
 
     if left:
         world.move_player(-10, 0)
@@ -130,8 +152,5 @@ while True:
         world.move_player(0, 10)
         if not world.subrect(playerect):
             world.move_player(0, -10)
-    if "-h" in sys.argv or "--hitboxes" in sys.argv:
-        pygame.draw.rect(canvas, (0, 255, 0), playerect, 1)
-        world.show_hitboxes()
     pygame.display.update()
     clock.tick(45)
